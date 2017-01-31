75904ec45c9fe1b3fe5363d15198278d68051147
syrop <wiktor.nizio@gmail.com>
2017-01-31 21:23:48 +0100

    Added the first C++ file.

---
+++ texter/src/main/cpp/native-lib.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wiktor on 31.01.17.
+//
+
+#include "native-lib.h"
---
+++ texter/src/main/cpp/native-lib.h
@@ -0,0 +1,8 @@
+//
+// Created by wiktor on 31.01.17.
+//
+
+#ifndef GPS_TEXTER_NATIVE_LIB_H
+#define GPS_TEXTER_NATIVE_LIB_H
+
+#endif //GPS_TEXTER_NATIVE_LIB_H
