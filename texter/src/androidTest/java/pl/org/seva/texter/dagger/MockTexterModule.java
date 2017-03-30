eef0f0d4dd627ad28e9488eb91a05b7ab6afb10a
syrop <wiktor.nizio@gmail.com>
2017-03-30 21:20:05 +0200

    Corrected mocking GpsTexter.

--- texter/src/androidTest/java/pl/org/seva/texter/dagger/MockTexterModule.java
+++ texter/src/androidTest/java/pl/org/seva/texter/dagger/MockTexterModule.java
@@ -1,19 +1,36 @@
 package pl.org.seva.texter.dagger;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import org.mockito.Mockito;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import io.reactivex.subjects.PublishSubject;
 import pl.org.seva.texter.manager.GpsManager;
+import pl.org.seva.texter.preference.HomeLocationPreference;
+import pl.org.seva.texter.utils.Constants;
 
 @Module
-public class MockTexterModule {
+class MockTexterModule {
 
     @Provides
     @Singleton
     GpsManager provideGpsManager() {
-        return Mockito.mock(GpsManager.class);
+        GpsManager result = Mockito.mock(GpsManager.class);
+        mockReturnValues(result);
+        return result;
+    }
+
+    private void mockReturnValues(GpsManager gpsManager) {
+        String defaultHomeLocation = Constants.DEFAULT_HOME_LOCATION;
+        double homeLat = HomeLocationPreference.parseLatitude(defaultHomeLocation);
+        double homeLon = HomeLocationPreference.parseLongitude(defaultHomeLocation);
+
+        Mockito.when(gpsManager.getHomeLatLng()).thenReturn(new LatLng(homeLat, homeLon));
+        Mockito.when(gpsManager.providerEnabledListener()).thenReturn(PublishSubject.create());
+        Mockito.when(gpsManager.providerDisabledListener()).thenReturn(PublishSubject.create());
     }
 }
