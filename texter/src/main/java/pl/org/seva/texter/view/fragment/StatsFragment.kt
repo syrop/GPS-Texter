f2879b1de7a86d86a70d377f44db9f70c3e8761b
syrop <wiktor.nizio@gmail.com>
2017-06-03 20:35:29 +0200

    Refactoring: Replaced star import.

--- texter/src/main/java/pl/org/seva/texter/view/fragment/StatsFragment.kt
+++ texter/src/main/java/pl/org/seva/texter/view/fragment/StatsFragment.kt
@@ -29,10 +29,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapFragment
-import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
