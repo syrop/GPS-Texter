7a914d41270cb02c4086d25ae21bec4f69f82763
syrop <wiktor.nizio@gmail.com>
2017-06-10 20:34:23 +0200

    Corrected ongoing notification handling in O.

--- texter/src/main/java/pl/org/seva/texter/presenter/service/TexterService.kt
+++ texter/src/main/java/pl/org/seva/texter/presenter/service/TexterService.kt
@@ -32,6 +32,11 @@ import pl.org.seva.texter.presenter.source.LocationSource
 import pl.org.seva.texter.presenter.utils.SmsSender
 import pl.org.seva.texter.view.activity.MainActivity
 import pl.org.seva.texter.TexterApplication
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+
 
 class TexterService : Service() {
 
@@ -41,6 +46,7 @@ class TexterService : Service() {
     lateinit var smsSender: SmsSender
 
     private var distanceSubscription = Disposables.empty()
+    private val notificationBuilder by lazy { createNotificationBuilder() }
 
     override fun onBind(arg0: Intent): IBinder? {
         return null
@@ -65,8 +71,7 @@ class TexterService : Service() {
                 System.currentTimeMillis().toInt(),
                 mainActivityIntent,
                 0)
-        @Suppress("DEPRECATION")
-        return Notification.Builder(this)
+        return notificationBuilder
                 .setContentTitle(getString(R.string.app_name))
                 .setSmallIcon(
                         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
@@ -76,6 +81,34 @@ class TexterService : Service() {
                 .setContentIntent(pi)
                 .setAutoCancel(false)
                 .build()
+    }
+
+    private fun createNotificationBuilder() : Notification.Builder {
+        return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            @Suppress("DEPRECATION")
+            Notification.Builder(this)
+        }
+        else {
+            val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            // The id of the channel.
+            val id = "my_channel_01"
+            // The user-visible name of the channel.
+            val name = getString(R.string.channel_name)
+            // The user-visible description of the channel.
+            val description = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_LOW
+            val mChannel = NotificationChannel(id, name, importance)
+            // Configure the notification channel.
+            mChannel.description = description
+            mChannel.enableLights(true)
+            // Sets the notification light color for notifications posted to this
+            // channel, if the device supports this feature.
+            mChannel.lightColor = Color.RED
+            mChannel.enableVibration(true)
+            mChannel.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
+            mNotificationManager.createNotificationChannel(mChannel)
+            Notification.Builder(this, id)
+        }
     }
 
     override fun onDestroy() {
