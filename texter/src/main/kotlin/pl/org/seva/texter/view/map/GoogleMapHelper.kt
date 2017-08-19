ea58e35df3b7d75bce3fa2183144a429167f4527
syrop <wiktor.nizio@gmail.com>
2017-08-19 17:34:53 +0200

    Corrected preparing map fragment.

--- texter/src/main/kotlin/pl/org/seva/texter/view/map/GoogleMapHelper.kt
+++ texter/src/main/kotlin/pl/org/seva/texter/view/map/GoogleMapHelper.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2017 Wiktor Nizio
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package pl.org.seva.texter.view.map
 
 import android.support.v4.app.FragmentManager
@@ -7,7 +24,7 @@ import com.google.android.gms.maps.SupportMapFragment
 fun prepareMap(f: GoogleMapHelper.() -> Unit): SupportMapFragment = GoogleMapHelper().apply(f).prepare()
 
 infix fun SupportMapFragment.ready(f: GoogleMap.() -> Unit) {
-
+    getMapAsync(f)
 }
 
 class GoogleMapHelper {
@@ -24,6 +41,4 @@ class GoogleMapHelper {
 
         return result!!
     }
-
-
 }
